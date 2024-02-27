33565128e943fe4c8d86b421944ee693d0261730
anishdiwan <anishdiwan@diwan-ubuntu20.04>
2024-02-27 16:53:30 +0100

    Debug motion lib

--- isaacgymenvs/custom_envs/motion_lib.py
+++ isaacgymenvs/custom_envs/motion_lib.py
@@ -83,12 +83,13 @@ def pair_data(data_dict, episode_ends, num_amp_obs_steps, num_amp_obs_per_step, 
 
 
 class MotionDataset():
-    def __init__(self, motion_file, num_amp_obs_steps, num_amp_obs_per_step, device=None):
+    def __init__(self, motion_file, num_amp_obs_steps, num_amp_obs_per_step, device=None, normalize=False):
         if device == None:
             self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         else:
             self.device = device
         
+        self.normalize = normalize
         self.num_amp_obs_steps = num_amp_obs_steps
         self.num_amp_obs_per_step = num_amp_obs_per_step
         self._load_motions(motion_file)
@@ -109,42 +110,52 @@ class MotionDataset():
         # Marks one-past the last index for each episode
         episode_ends = dataset_root['meta']['episode_ends'][:]
 
-        # compute statistics and normalized data to [-1,1]
-        stats = dict()
-        normalized_train_data = dict()
-        for key, data in train_data.items():
-            stats[key] = get_data_stats(data)
-            normalized_train_data[key] = normalize_data(data, stats[key])
-        paired_normalized_data, paired_normalised_episodes = pair_data(normalized_train_data, episode_ends, self.num_amp_obs_steps, self.num_amp_obs_per_step, self.device)
+        if self.normalize:
+            # compute statistics and normalized data to [-1,1]
+            stats = dict()
+            processed_train_data = dict()
+            for key, data in train_data.items():
+                stats[key] = get_data_stats(data)
+                processed_train_data[key] = normalize_data(data, stats[key])
 
-        self.stats = stats
-        self.normalized_train_data = normalized_train_data
-        self.paired_normalized_data = paired_normalized_data
-        self.paired_normalised_episodes = paired_normalised_episodes
-        self.num_episodes = len(paired_normalised_episodes)
+            self.stats = stats
+            self.processed_train_data = processed_train_data
+
+        else:
+            processed_train_data = train_data
+                
+        paired_processed_data, paired_processed_episodes = pair_data(processed_train_data, episode_ends, self.num_amp_obs_steps, self.num_amp_obs_per_step, self.device)
+
+
+        self.paired_processed_data = paired_processed_data
+        self.paired_processed_episodes = paired_processed_episodes
+        self.num_episodes = len(paired_processed_episodes)
 
     def __len__(self):
         # all possible segments of the dataset
-        return len(self.paired_normalized_data)
+        return len(self.paired_processed_data)
 
     def __getitem__(self, idx):
-        # sample = self.paired_normalized_data[idx]
+        # sample = self.paired_processed_data[idx]
         # return sample
 
         assert self.episode != None, "Please select an episode in the dataloader sample method"
         # try:
-        sample = self.paired_normalised_episodes[self.episode][idx + self.offset]
+        sample = self.paired_processed_episodes[self.episode][idx + self.offset]
         # except IndexError as e:
-        #     print(self.paired_normalised_episodes[self.episode])
+        #     print(self.paired_processed_episodes[self.episode])
         #     raise e
 
         return sample
 
 class MotionLib():
-    def __init__(self, motion_file, num_amp_obs_steps, num_amp_obs_per_step, device=None):
-        self.dataset = MotionDataset(motion_file, num_amp_obs_steps, num_amp_obs_per_step, device=device)
-        self.dataloader = None
+    def __init__(self, motion_file, num_amp_obs_steps, num_amp_obs_per_step, device=None, normalize=False):
+        # By default the dataset is normalised. If not needed, it is unnormalized here. 
+        # NOTE: AMP also normalizes data internally. It is hence advisable to set normalization to false while sampling and let AMP handle it internally
+        self.normalize = normalize
 
+        self.dataset = MotionDataset(motion_file, num_amp_obs_steps, num_amp_obs_per_step, device=device, normalize=normalize)
+        self.dataloader = None
 
     def sample_motions(self, num_samples):
         # TODO: sample considering episode ends
@@ -154,12 +165,13 @@ class MotionLib():
         ep_found = False
         while not ep_found:
             random_episode_idx = random.randrange(self.dataset.num_episodes)
-            if len(self.dataset.paired_normalised_episodes[random_episode_idx]) > num_samples:
+            if len(self.dataset.paired_processed_episodes[random_episode_idx]) > num_samples:
                 ep_found = True
         self.dataset.episode = random_episode_idx
-        self.dataset.offset = random.randrange((len(self.dataset.paired_normalised_episodes[random_episode_idx])) - num_samples)
+        self.dataset.offset = random.randrange((len(self.dataset.paired_processed_episodes[random_episode_idx])) - num_samples)
 
         batch = next(iter(self.dataloader))
+
         return batch
 
     def _setup_dataloader(self, batch_size):
