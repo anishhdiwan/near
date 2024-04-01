441e1701b609f159668288f8e5b34bef3425430c
anishdiwan <anishdiwan@diwan-ubuntu20.04>
2024-04-01 19:00:23 +0200

    Save and visualise motions
    Also test all data recording cases like A-only, A+B, A+A

--- isaacgymenvs/custom_envs/test_maze.py
+++ isaacgymenvs/custom_envs/test_maze.py
@@ -2,8 +2,8 @@ from maze_env import MazeEnv
 import time
 import numpy as np
 
-env = MazeEnv()
-env.teleop_agent(record_data=True)
+# env = MazeEnv()
+# env.teleop_agent(record_data=True)
 
 
 # env = MazeEnv(normalise_action=True)
@@ -18,3 +18,26 @@ env.teleop_agent(record_data=True)
 # env.close()
 
 
+## Visualising Saved Motions ##
+# _01-18-50-00.npy
+# _01-18-50-14.npy
+# _01-18-50-43.npy
+
+# states = np.load('data/maze_env/_01-18-50-00.npy')
+# print(len(states))
+
+# env = MazeEnv()
+# obs = env.reset()
+
+# dt = 1.0 / env.sim_hz
+# n_steps = env.sim_hz // env.control_hz
+
+
+# for pos in states:
+
+#     for i in range(n_steps):
+#         env.agent.position = pos.tolist()
+#         # Step physics.
+#         env.space.step(dt)
+        
+#     env.render(mode="human")
