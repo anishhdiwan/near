499140319215fbb91bf6080373fc6fab4ad48732
anishdiwan <anishdiwan@diwan-ubuntu20.04>
2024-06-10 17:16:54 +0200

    Seed NCSN; Develop UI for motion viewing

--- isaacgymenvs/tasks/amp/poselib/fbx_motion_to_npy.py
+++ isaacgymenvs/tasks/amp/poselib/fbx_motion_to_npy.py
@@ -6,48 +6,99 @@ This script takes in a dataset of motions in .fbx format, then tranforms them to
 This is done based on some given task and an the motion index
 """
 
-
 import os
 import sys
 import pandas as pd
+import argparse
 pd.options.mode.chained_assignment = None  # default='warn'
 
 from poselib.skeleton.skeleton3d import SkeletonTree, SkeletonState, SkeletonMotion
 from poselib.visualization.common import plot_skeleton_state, plot_skeleton_motion_interactive
-
 from pathlib import Path
-home = str(Path.home())
-# fbx data path
-data_path = home + "/thesis_background/Datasets/CMU_humanoid_fbx/"
 
 
-data_index = pd.read_csv(data_path + "cmu_mocap_task_index.csv")
-task_name = ['forward jump']
-task_index = data_index.loc[data_index['task'].isin(task_name)]
-task_index['motion_file'] = data_path + "CMU_fbx/" + task_index['motion_index'] + ".fbx"
-motion_files = task_index['motion_file'].to_list()
-motion_indices = task_index['motion_index'].to_list()
+if __name__ == "__main__":
 
-savepath = data_path + "cmu_" + task_name[0] + "_task/" 
-if not os.path.exists(savepath):
-    os.makedirs(savepath)
+    print("""
 
-print(f"Number of mo-cap files for the {task_name[0]} task: {len(motion_files)}")
+    This script converts .fx format motion files to .npy format files. It also has an option to just visualise a motion task demostration data.
+    
+    Run with the option --help for more info
 
-for idx, fbx_file in enumerate(motion_files):
-    print(f"Loading {fbx_file}")
-    # import fbx file - make sure to provide a valid joint name for root_joint
-    motion = SkeletonMotion.from_fbx(
-        fbx_file_path=fbx_file,
-        root_joint="Hips",
-        fps=60,
-    )
+    Available options for the task argument can be found in the "task" column of the cmu-mocap-index sheet (https://docs.google.com/spreadsheets/d/1ZW5a8w7mewPjhc0sIlnG2uqvQRUIQV_X5Ps9RxQlD20/edit?usp=sharing)
+    Note: The passed path must have the "cmu_mocap_task_index.csv" file
 
-    # save motion in npy format
-    file_name = savepath + "cmu_" + motion_indices[idx] + ".npy"
-    motion.to_file(file_name)
+    """)
 
-    # visualize motion
-    # plot_skeleton_motion_interactive(motion)
+    parser = argparse.ArgumentParser() 
 
-    print("-----------------")
+    parser.add_argument("-t", "--task", type=str, default="walk", required=False, help="name of the task to convert/view")
+    parser.add_argument("-ns", "--no_save", default=True, action='store_false', required=False, help="when not to save the converted .npy files")
+    parser.add_argument("-pv", "--preview", default=False, action='store_true', required=False, help="preview the first few motion files")
+    parser.add_argument("-v", "--view", default=False, action='store_true', required=False, help="view all motion files")
+    parser.add_argument("-spth", "--save_path", type=str, default="/thesis_background/Datasets/CMU_humanoid_fbx/", required=False, help="path to save the data (relative to home)")
+    args = parser.parse_args()
+
+    save = args.no_save
+    view = args.view
+    preview = args.preview
+    
+    if preview == True:
+        if view == True or save == True:
+            print("Warning! Cannot view all motions or save motions if preview is set to True. Proceeding to preview")
+            view = False
+            save = False
+    else:
+        if view == True and save == True:
+            print("Warning! Cannot view all motions and save motions at the same time. Proceeding to save")
+            view = False
+
+    if save == False and view == False and preview == False:
+        print("Warning! No feasible options passed. Previewing motions")
+        preview = True
+
+
+    home = str(Path.home())
+    # fbx data path
+    data_path = home + args.save_path
+
+
+    data_index = pd.read_csv(data_path + "cmu_mocap_task_index.csv")
+    task_name = [args.task]
+    task_index = data_index.loc[data_index['task'].isin(task_name)]
+    task_index['motion_file'] = data_path + "CMU_fbx/" + task_index['motion_index'] + ".fbx"
+    motion_files = task_index['motion_file'].to_list()
+    motion_indices = task_index['motion_index'].to_list()
+
+    savepath = data_path + "cmu_" + task_name[0] + "_task/" 
+    if save:
+        if not os.path.exists(savepath):
+            os.makedirs(savepath)
+
+    print(f"Number of mo-cap files for the {task_name[0]} task: {len(motion_files)}")
+
+    if preview:
+        motion_files = motion_files[:2]
+
+
+
+
+    for idx, fbx_file in enumerate(motion_files):
+        print(f"Loading {fbx_file}")
+        # import fbx file - make sure to provide a valid joint name for root_joint
+        motion = SkeletonMotion.from_fbx(
+            fbx_file_path=fbx_file,
+            root_joint="Hips",
+            fps=60,
+        )
+
+        # save motion in npy format
+        if save:
+            file_name = savepath + "cmu_" + motion_indices[idx] + ".npy"
+            motion.to_file(file_name)
+
+        # visualize motion
+        if view or preview:
+            plot_skeleton_motion_interactive(motion)
+
+        print("-----------------")
--- isaacgymenvs/train_ncsn.py
+++ isaacgymenvs/train_ncsn.py
@@ -12,11 +12,12 @@ from datetime import datetime
 import argparse
 # from utils.ncsn_utils import dict2namespace
 
-@hydra.main(version_base="1.1", config_name="gym_env_config", config_path="./cfg")
+@hydra.main(version_base="1.1", config_name="config", config_path="./cfg")
 def launch_hydra(cfg: DictConfig):
 
     from learning.motion_ncsn.runners.anneal_runner import AnnealRunner
     from isaacgymenvs.utils.reformat import omegaconf_to_dict, print_dict
+    from isaacgymenvs.utils.utils import set_np_formatting, set_seed
 
     # ensure checkpoints can be specified as relative paths
     # if cfg.checkpoint:
@@ -29,6 +30,15 @@ def launch_hydra(cfg: DictConfig):
     device = dmp_cfg.get('device', 'cuda:0')
     with open_dict(dmp_cfg):
         dmp_cfg.device = device
+
+
+    # set numpy formatting for printing only
+    set_np_formatting()
+    # global rank of the GPU
+    global_rank = int(os.getenv("RANK", "0"))
+    # sets seed. if seed is -1 will pick a random one
+    with open_dict(dmp_cfg):
+        dmp_cfg.seed = set_seed(cfg.seed, torch_deterministic=cfg.torch_deterministic, rank=global_rank)
 
 
     # dump config dict
